b76ef5cc0dc670c8378c18f5363f68b42916d001
katbray <katherinebraynard@gmail.com>
2025-11-22 15:49:36 +0000

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({base: "/Oxford-Sacred-Footsteps/",
+export default defineConfig(({ mode }) => ({
+  base: "/Oxford-Sacred-Footsteps/",
   server: {
     host: "::",
     port: 8080,
